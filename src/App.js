f00326fc00756db86b0bb2b0ed8a41164d7039d2
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-04 20:46:12 +1000

    v3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Home from "./container/Home";
 import Login from "./components/Login";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
+  }, []);
   return (
     <Routes>
       <Route path="login" element={<Login />} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
-import shareVideo from "../assets/share.mp4";
+import share from "../assets/bg.jpeg";
 import logo from "../assets/logowhite.png";
 
 import { client } from "../client";
@@ -29,14 +29,11 @@ const Login = () => {
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
-        <video
-          src={shareVideo}
-          type="video/mp4"
-          loop
+        <img
+          src={share}
           controls={false}
-          muted
-          autoPlay
           className="w-full h-full object-cover"
+          alt='bg'
         />
 
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
